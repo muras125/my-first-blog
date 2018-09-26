bd3f38e309dba74ebc79886394f8ca2241cc48c6
niitmuralim <niitmuralim@cpsl.com.hk>
2018-09-26 16:40:40 +0800

    Modified template to display post from database, added sample index page

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index, name='index'),
+	path('profile', views.index, name='index'),
 	path('blogs', views.post_list, name='post_list'),
 ]
