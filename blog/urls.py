679b10b352090333963c46354184dfe25f2c04e3
niitmuralim <niitmuralim@cpsl.com.hk>
2018-09-26 17:17:45 +0800

    Modified template to display post from database, added sample index page

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	path('profile', views.index, name='index'),
 	path('blogs', views.post_list, name='post_list'),
+	path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
@@ -9,3 +10,9 @@ def post_list(request):
 	
 def index(request):
 	return render(request, 'blog/index.html', {})
+	
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
+	
+	
