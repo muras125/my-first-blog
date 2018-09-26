c82d38e1693fc0282533945d566d5709f4c69c52
niitmuralim <niitmuralim@cpsl.com.hk>
2018-09-26 17:33:41 +0800

    Modified template to display post from database, added sample index page

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	path('profile', views.index, name='index'),
 	path('blogs', views.post_list, name='post_list'),
 	path('post/<int:pk>/', views.post_detail, name='post_detail'),
+	
 ]
